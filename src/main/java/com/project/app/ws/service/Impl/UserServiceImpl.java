a42a499f6130a839abf34341a02e4579469a16d7
Geraldo <geraldo.shkembi@fshnstudent.info>
2021-03-12 12:45:23 +0100

    Initial Commit

---
+++ src/main/java/com/project/app/ws/service/Impl/UserServiceImpl.java
@@ -0,0 +1,131 @@
+package com.project.app.ws.service.Impl;
+
+import com.project.app.shared.Utils;
+import com.project.app.shared.dto.AddressDTO;
+import com.project.app.shared.dto.UserDto;
+import com.project.app.ui.model.response.ErrorMessages;
+import com.project.app.ws.exceptions.UserServiceException;
+import com.project.app.ws.io.repositories.UserRepository;
+import com.project.app.ws.io.entity.UserEntity;
+import com.project.app.ws.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+
+        for(int i=0; i < user.getAddresses().size(); i++) {
+            AddressDTO address = user.getAddresses().get(i);
+            address.setUserDetails(user);
+            address.setAddressId(utils.generateAddressId(30));
+            user.getAddresses().set(i,address);
+        }
+
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity userEntity = modelMapper.map(user,UserEntity.class);
+
+        userEntity.setUserId(utils.generateUserId(30));
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = modelMapper.map(storedUserDetails,UserDto.class);
+        return returnValue;
+    }
+
+    @Override
+    public UserDto getUser(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if(userEntity == null) throw new UsernameNotFoundException(email);
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(userEntity,returnValue);
+        //ModelMapper modelMapper = new ModelMapper();
+       // UserDto returnValue = modelMapper.map(userEntity,UserDto.class);
+        return returnValue;
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserDto returnValue = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if(userEntity == null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+        ModelMapper modelMapper = new ModelMapper();
+        returnValue = modelMapper.map(userEntity,UserDto.class);
+        return returnValue;
+    }
+
+    @Override
+    public UserDto updateUser(String userId,UserDto user) {
+        ModelMapper modelMapper = new ModelMapper();
+
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if(userEntity == null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+        userEntity.setFirstName(user.getFirstName());
+        userEntity.setLastName(user.getLastName());
+
+        UserEntity updatedUserDetails = userRepository.save(userEntity);
+        UserDto returnValue = modelMapper.map(updatedUserDetails,UserDto.class);
+
+        return returnValue;
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if(userEntity == null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+        userRepository.delete(userEntity);
+    }
+
+    @Override
+    public List<UserDto> getUsers(int page, int limit) {
+
+        if(page > 0) page++;
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        List<UserDto> returnValue = new ArrayList<>();
+        PageRequest pageableRequest = PageRequest.of(page,limit);
+        Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+        List<UserEntity> users = usersPage.getContent();
+        for(UserEntity userEntity: users) {
+            UserDto userDto = modelMapper.map(userEntity,UserDto.class);
+
+            returnValue.add(userDto);
+        }
+        return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if(userEntity == null) throw new UsernameNotFoundException(email);
+        return new User(userEntity.getEmail(),userEntity.getEncryptedPassword(), new ArrayList<>());
+    }
+}
